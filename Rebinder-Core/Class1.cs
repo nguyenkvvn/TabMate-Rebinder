988efe9d54f595a7d0b5ee0f5316f49ecf641eba
Vinh Nguyen <kevin@itske.vin>
2019-07-06 00:53:07 -0400

    Initial commit

---
+++ Rebinder-Core/Class1.cs
@@ -0,0 +1,70 @@
+﻿using SlimDX.DirectInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rebinder_Core
+{
+    /// <summary>
+    /// The TabMateDevice class is derived from the DirectInput device class. It will handle finding a TabMate device, along with raising events to hook into.
+    /// </summary>
+    public class TabMateDevice
+    {
+        //Microsoft.DirectX.DirectInput.Device Tabmate;
+
+        /// <summary>
+        /// This is the timer that fires to capture button presses.
+        /// </summary>
+        public System.Timers.Timer event_timer = new System.Timers.Timer();
+
+        /// The target "joystick"- the TabMate
+        SlimDX.DirectInput.Joystick tab_mate;
+
+        public TabMateDevice()
+        {
+            // Find the device
+            /// The DirectInput interface
+            SlimDX.DirectInput.DirectInput input = new SlimDX.DirectInput.DirectInput();
+            
+            /// For every device connected using DirectInput that:
+            /// -   is a "game controller"
+            /// -   and is connected
+            foreach (DeviceInstance device in input.GetDevices(DeviceClass.GameController, DeviceEnumerationFlags.AttachedOnly))
+            {
+                /// Get the TabMate from the input interface, and bind its ref via the guid
+                tab_mate = new Joystick(input, device.InstanceGuid);
+                break;
+            }
+
+            event_timer.Elapsed += Event_timer_Elapsed;
+            event_timer.Enabled = true;
+            event_timer.Interval = 1;
+
+        }
+
+        private void Event_timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            StickHandlerLogic(tab_mate);
+        }
+
+        void StickHandlerLogic(Joystick js)
+        {
+            //Enumerate a snapshot of the joystick
+            JoystickState state = new JoystickState();
+            tab_mate.Acquire();
+            state = tab_mate.GetCurrentState();
+
+            bool[] buttons = state.GetButtons();
+
+            for (int button_num = 0; button_num < buttons.Length; button_num++)
+            {
+                if (buttons[button_num])
+                {
+                    Console.WriteLine("Button " + button_num + " was pressed!");
+                }
+            }
+        }
+    }
+}
---
+++ Rebinder-Tester/Program.cs
@@ -0,0 +1,109 @@
+﻿using Rebinder_Core;
+using SlimDX.DirectInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rebinder_Tester
+{
+    class Program
+    {
+        //Joystick[] Sticks;
+
+        //System.Timers.Timer timer1;
+
+        static void Main(string[] args)
+        {
+
+            TabMateDevice tmd = new TabMateDevice();
+
+            while (tmd.event_timer.Enabled)
+            {
+
+            }
+
+            /*List<SlimDX.DirectInput.Joystick> sticks = new List<SlimDX.DirectInput.Joystick>();
+
+            SlimDX.DirectInput.DirectInput Input = new DirectInput();
+            SlimDX.DirectInput.Joystick stick;
+
+            System.Timers.Timer timer1 = new System.Timers.Timer
+            {
+                Enabled = true,
+                Interval = 1
+            };
+
+            timer1.Elapsed += timer1_Tick;
+
+            foreach (DeviceInstance device in Input.GetDevices(DeviceClass.GameController, DeviceEnumerationFlags.AttachedOnly))
+            {
+                // Creates a joystick for each game device in USB Ports
+                try
+                {
+                    stick = new SlimDX.DirectInput.Joystick(Input, device.InstanceGuid);
+                    stick.Acquire();
+
+                    // Gets the joysticks properties and sets the range for them.
+                    foreach (DeviceObjectInstance deviceObject in stick.GetObjects())
+                    {
+                        if ((deviceObject.ObjectType & ObjectDeviceType.Axis) != 0)
+                            stick.GetObjectPropertiesById((int)deviceObject.ObjectType).SetRange(-100, 100);
+                    }
+
+                    // Adds how ever many joysticks are connected to the computer into the sticks list.
+                    Sticks.Add(stick);
+                }
+                catch (DirectInputException)
+                {
+                }
+            }
+            Console.WriteLine(sticks.Count);
+            //return sticks.ToArray();*/
+
+
+        }
+
+        /*private static void Timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Creates the StickHandlingLogic Method which takes all the joysticks in the sticks List and puts them into a timer.
+        public static void timer1_Tick(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            for (int i = 0; i < Sticks.Length; i++)
+            {
+                StickHandlingLogic(Sticks[i], i);
+            }
+        }
+
+        void StickHandlingLogic(Joystick stick, int id)
+        {
+            // Creates an object from the class JoystickState.
+            JoystickState state = new JoystickState();
+
+            state = stick.GetCurrentState(); //Gets the state of the joystick
+
+            //These are for the thumbstick readings
+            /*yValue = -state.Y;
+            xValue = state.X;
+            zValue = state.Z;
+            rotationZValue = -state.RotationZ;*/
+
+            //bool[] buttons = state.GetButtons(); // Stores the number of each button on the gamepad into the bool] butons.
+            //Here is an example on how to use this for the joystick in the first index of the array list
+            /*if (id == 0)
+            {
+                // This is when button 0 of the gamepad is pressed, the label will change. Button 0 should be the square button.
+                if (buttons[0])
+                {
+                    label1.Text = "Worked";
+                }
+
+            }
+        }*/
+    }
+}
